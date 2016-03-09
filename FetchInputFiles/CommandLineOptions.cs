424023ecb91dd686f81855ad5e82562042218d9f
Sridhar Sathya <sridhar.sathya@live.com>
2016-03-09 13:04:19 -0500

    Completed FetchInputFiles. Need to add logging and error checks.

---
+++ FetchInputFiles/CommandLineOptions.cs
@@ -0,0 +1,16 @@
+﻿using CommandLine;
+
+namespace FetchInputFiles
+{
+    public class CommandLineOptions
+    {
+        [Option('u', "url", Required = true, HelpText = "FTP URL to get files from")]
+        public string SourceUrl { get; set; }
+
+        [Option('d', "dir", Required = true, HelpText = "Folder where to drop files")]
+        public string DestinationFolder { get; set; }
+
+        [Option('f', "listFile", Required = true, HelpText = "List of files to download")]
+        public string FilesList { get; set; }
+    }
+}
---
+++ FetchInputFilesTests/FetchFilesTests.cs
@@ -0,0 +1,44 @@
+﻿using FetchInputFiles;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace FetchInputFilesTests
+{
+    [TestClass()]
+    public class FetchFilesTests
+    {
+        private FetchFiles ff;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            ff = new FetchFiles()
+            {
+                CommandLineArguments = new List<string>()
+                {
+                    "-d",
+                    System.IO.Path.GetTempPath(),
+                    "-u",
+                    "ftp://ftp.funet.fi/pub/mirrors/ftp.imdb.com/pub/",
+                    "-f",
+                    @"c:\users\sridhar\documents\visual studio 2013\Projects\IMDBMongo\FetchInputFiles\Data\FileList.txt"
+                }
+            };
+        }
+
+        [TestMethod()]
+        public void ProcessCommandLineArgumentsTest()
+        {
+            ff.ProcessCommandLineArguments();
+            Assert.AreEqual(ff.CommandLineOpts.SourceUrl, @"ftp://ftp.funet.fi/pub/mirrors/ftp.imdb.com/pub/");
+        }
+
+        [TestMethod]
+        public void CheckExternalFilesDownloadedTest()
+        {
+            ff.ProcessCommandLineArguments();
+            var count = ff.GetNumberOfFilesDownloaded();
+            Assert.AreEqual(count, 47);
+        }
+    }
+}
