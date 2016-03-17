25b0ec53842b98b0206bfff6cb9f74379760f302
Sridhar Sathya <sridhar.sathya@live.com>
2016-03-17 15:36:27 -0400

    Completed taglist.list
    Related Work Items: #208

--- PrepareInputFiles/Parsers/TagLinesParser.cs
+++ PrepareInputFiles/Parsers/TagLinesParser.cs
@@ -1,19 +1,23 @@
-﻿using DataModel;
+﻿using Anotar.NLog;
+using DataModel;
+using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace PrepareInputFiles.Parsers
 {
     public class TagLinesParser : FileParser
     {
-        private readonly List<TagLine> _records;
+        private List<TagLine> _records;
 
         public TagLinesParser(string sourceFile)
         {
             SourceFile = sourceFile;
-            HeaderLine = "-------------------------";
-            PreHeaderLine1 = "";
-            PreHeaderLine2 = "==========================";
+            HeaderLine = "-----------------------------------------------------------------------------";
+            PreHeaderLine1 = "TAG LINES LIST";
+            PreHeaderLine2 = "==============";
             RegularList = new List<string>
             {
                 @"(?s)(?<=\n)#.*?(?=\n\n)"
@@ -25,12 +29,42 @@ namespace PrepareInputFiles.Parsers
 
         public override bool ParseFile(string destinationFile)
         {
-            throw new System.NotImplementedException();
+            LogTo.Debug("\n\tBeign Parsing file");
+            ReadRecords();
+            LogTo.Debug("\n\tEnd Parsing input file");
+            _records = _records.OrderBy(m => m.MovieName).ThenBy(y => y.Year).ToList();
+            File.WriteAllText(destinationFile, JsonConvert.SerializeObject(_records, Formatting.Indented));
+            LogTo.Debug("Output JSON to {0}", destinationFile);
+            return _records.Any();
         }
 
         protected override void PopulateRecords(IEnumerable<Match> lines)
         {
-            throw new System.NotImplementedException();
+            foreach (var line in lines)
+            {
+                var rawRecord = line.ToString().Split('\n');
+                var record = new TagLine();
+                var movieName = rawRecord.FirstOrDefault(m => m.StartsWith("#"));
+                if (movieName != null)
+                {
+                    movieName = movieName.Replace('#', ' ');
+                    FixMovieNames(record, movieName.Trim());
+                }
+                foreach (var str in rawRecord.Where(s => !s.StartsWith("#")))
+                {
+                    record.TagLines.Add(str.Trim());
+                }
+                //foreach (var tagLine in
+                //    _records.Where(tagLine => (MovieBase)tagLine ==
+                //        ((MovieBase)record)))
+                //{
+                //    foreach (var str in record.TagLines)
+                //        tagLine.TagLines.Add(str);
+                //    record.TagLines.Clear();
+                //}
+                if (record.TagLines.Any())
+                    _records.Add(record);
+            }
         }
     }
 }
---
+++ PrepareInputFilesTests/Parsers/TagLinesParserTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrepareInputFiles.Parsers;
+
+namespace PrepareInputFilesTests.Parsers
+{
+    [TestClass()]
+    public class TagLinesParserTests
+    {
+        private TagLinesParser _tagLinesParser;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _tagLinesParser = new TagLinesParser(@"c:\Users\Sridhar\Downloads\Delme\IMDB\taglines.list");
+        }
+
+        [TestMethod()]
+        public void ParseFileTest()
+        {
+            var parseResult = _tagLinesParser.ParseFile(@"c:\Users\Sridhar\Downloads\Delme\IMDB\taglines.json");
+            Assert.AreEqual(true, parseResult);
+        }
+    }
+}
