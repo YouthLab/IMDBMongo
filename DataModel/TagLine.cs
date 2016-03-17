fd7e741290c4b4f6f8ec554ed5fcee08b95ad20c
Sridhar Sathya <sridhar.sathya@live.com>
2016-03-17 11:04:36 -0400

    Added TagLine Class to model

---
+++ DataModel/TagLine.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace DataModel
+{
+    public class TagLine : MovieBase
+    {
+        public TagLine()
+        {
+            TagLines = new List<string>();
+        }
+
+        public List<string> TagLines { get; set; }
+    }
+}
