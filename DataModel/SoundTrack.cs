efe406652ceea884a07f05fd223e5f16b68b2565
Sridhar Sathya <sridhar.sathya@live.com>
2016-03-21 17:16:18 -0400

    Added missing files

---
+++ DataModel/SoundTrack.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace DataModel
+{
+    public class SoundTrack : MovieBase
+    {
+        public SoundTrack()
+        {
+            Titles = new List<string>();
+        }
+
+        public List<string> Titles { get; set; }
+        //dropping other details. The file has no standard format
+    }
+}
---
+++ PrepareInputFiles/Parsers/SoundTrackParser.cs
@@ -0,0 +1,60 @@
+﻿using Anotar.NLog;
+using DataModel;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PrepareInputFiles.Parsers
+{
+    public class SoundTrackParser : FileParser
+    {
+        private List<SoundTrack> _records { get; set; }
+
+        public SoundTrackParser(string sourceFile)
+        {
+            SourceFile = sourceFile;
+            HeaderLine = "-------------------------";
+            PreHeaderLine1 = "SOUNDTRACKS";
+            PreHeaderLine2 = "=============";
+            RegularList = new List<string>
+            {
+                @"(?s)(?<=\n)#.*?(?=\n\n)"
+            };
+            _records = new List<SoundTrack>();
+        }
+
+        public override bool ParseFile(string destinationFile)
+        {
+            LogTo.Debug("\n\tBegin Parsing files");
+            ReadRecords();
+            LogTo.Debug(("\n\tEnd Parsing input file"));
+            File.WriteAllText(destinationFile, JsonConvert.SerializeObject(_records.OrderBy(y => y.Year).
+                ThenBy(m => m.MovieName),
+                Formatting.Indented));
+            LogTo.Debug("Output JSON to {0}", destinationFile);
+            return _records.Any();
+        }
+
+        protected override void PopulateRecords(IEnumerable<Match> lines)
+        {
+            foreach (var line in lines)
+            {
+                var rawRecord = UtfStr(line).Split('\n');
+                rawRecord = rawRecord.Where(val => val != "").ToArray();
+                var record = new SoundTrack();
+
+                foreach (var s in rawRecord.Where(
+                    p => (p.StartsWith("#") || p.StartsWith("-"))))
+                {
+                    if (s.StartsWith("#"))
+                        FixMovieNames(record, s.Remove(0, 1));
+                    else
+                        record.Titles.Add(s.Remove(0, 1).Replace('\"', ' ').Trim());
+                }
+                _records.Add(record);
+            }
+        }
+    }
+}
