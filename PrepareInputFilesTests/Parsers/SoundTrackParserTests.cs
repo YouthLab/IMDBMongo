6a5ab4c93662e4a1c3988252ca866b269bc75fc3
Sridhar Sathya <sridhar.sathya@live.com>
2016-03-21 17:13:49 -0400

    Completed Sound tracks. Got confused between Sound-mix and soundtrack. As there are no denendencies between files it will not affect schedule.
    Related Work Items: #223

---
+++ PrepareInputFilesTests/Parsers/SoundTrackParserTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrepareInputFiles.Parsers;
+
+namespace PrepareInputFilesTests.Parsers
+{
+    [TestClass()]
+    public class SoundTrackParserTests
+    {
+        private SoundTrackParser soundTrackParser;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            soundTrackParser = new SoundTrackParser(@"c:\Users\Sridhar\Downloads\Delme\IMDB\soundtracks.list");
+        }
+
+        [TestMethod()]
+        public void ParseFileTest()
+        {
+            var parseResult = soundTrackParser.ParseFile(@"c:\Users\Sridhar\Downloads\Delme\IMDB\soundtracks.json");
+            Assert.AreEqual(true, parseResult);
+        }
+    }
+}
